f5273794fdfbbef8a266869356008265cf34c454
Hidenori Maehara <hmaehara@peacemind-jeap.co.jp>
2013-12-12 16:21:49 +0900

    add test

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -58,6 +58,14 @@ class QueryTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(array('hoge', 'man'), $binds);
     }
 
+    public function test_where_raw_with_single_value()
+    {
+        $query = \Kogatana\Query::table('users')->where_raw('name = ?', 'hoge');
+        list($sql, $binds) = $query->to_sql();
+        $this->assertEquals("SELECT\n  *\nFROM\n  users\nWHERE\n  name = ?", $sql);
+        $this->assertEquals(array('hoge'), $binds);
+    }
+
     public function test_join()
     {
         $query = \Kogatana\Query::table('users')->join('songs', array('users.id', '=', 'songs.user_id'));
